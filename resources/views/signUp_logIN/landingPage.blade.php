d6f681d7c18c5740419fb7ca1a9cf743f4694ed2
EmmanuelleSantos <hz202301270@wmsu.edu.ph>
2026-02-13 05:57:28 +0800

    LANDING PAGE WEB (UPDATED FONT)

--- resources/views/signUp_logIN/landingPage.blade.php
+++ resources/views/signUp_logIN/landingPage.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap">
     <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.4.0/uicons-regular-rounded/css/uicons-regular-rounded.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css">
     <link rel="stylesheet" href="{{ asset('css/signUp_logIN/landingPage.css') }}">
